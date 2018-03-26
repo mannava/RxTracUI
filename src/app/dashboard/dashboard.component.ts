1b09867abd7d1a3c8055fcea35d67635ffc0e221
bhaskar <bhaskar.mannava@virginamerica.com>
2018-03-26 14:45:12 -0700

    updated page views.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -27,7 +27,7 @@ export class DashboardComponent implements OnInit {
     };
 
     constructor(private dashboardService: DashboardService, private notificationsService: NotificationsService) {
-        this.getDashboardData('all');
+        this.getDashboardData('connect');
     }
 
     ngOnInit() {
